927cfa73346bccabc0d975a746a567d9d085b5a3
Loc Duong <myloc442@gmail.com>
2023-03-05 15:50:40 +0700

    variable javascript

---
+++ 01.ES6/1_variable.js
@@ -0,0 +1,30 @@
+// var has a function scope, not a block scope
+// var age = 18;
+
+// function showLog() {
+//   console.log("🚀 ~ age:", age);
+// }
+
+// showLog();
+
+// let has a block scope
+// let age = 17;
+// if (age > 18) {
+//   let mess = "Ban da du tuoi";
+//   console.log("In block: ", mess); // ok
+// } else {
+//   console.log("Out block", mess); // mess is not defined
+// }
+
+// const cannot change through re-assignment
+// const name = "dunlok";
+// name = "My Loc"; // Error: Assignment to constant varibale
+// console.log(name);
+
+// const has a block code ~ let
+
+// const person = {};
+// person.name = "Dunlok";
+// person.age = 18;
+
+// console.log(person);
