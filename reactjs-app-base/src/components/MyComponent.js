e482d33127f87740c355b225f389b328996f0d01
Loc Duong <myloc442@gmail.com>
2023-03-08 15:26:30 +0700

    constructor in class component

--- reactjs-app-base/src/components/MyComponent.js
+++ reactjs-app-base/src/components/MyComponent.js
@@ -3,13 +3,17 @@ import AddUserInfo from './AddUserInfo';
 import UserInfo from './UseInfo';
 
 class MyComponent extends React.Component {
-  state = {
-    listUser: [
-      { id: 1, name: 'Dương Mỹ Lộc', age: '21' },
-      { id: 2, name: 'Lê Thị Phương Nga', age: '20' },
-      { id: 3, name: 'Dương Mỹ Long', age: '15' },
-    ],
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listUser: [
+        { id: 1, name: 'Dương Mỹ Lộc', age: '21' },
+        { id: 2, name: 'Lê Thị Phương Nga', age: '20' },
+        { id: 3, name: 'Dương Mỹ Long', age: '15' },
+      ],
+    };
+  }
 
   handleAddNewUser = (userObj) => {
     this.setState({
