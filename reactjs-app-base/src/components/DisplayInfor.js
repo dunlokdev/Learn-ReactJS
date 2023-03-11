547792dc1c5996a640f74d8d57d2d71e584f894f
Loc Duong <myloc442@gmail.com>
2023-03-11 23:01:57 +0700

    Convert class component to functional component

--- reactjs-app-base/src/components/DisplayInfor.js
+++ reactjs-app-base/src/components/DisplayInfor.js
@@ -1,61 +1,96 @@
 import React from 'react';
 import './DisplayInfor.scss';
 
-class DisplayInfor extends React.Component {
-  state = {
-    isShow: true,
-  };
+// class DisplayInfor extends React.Component {
+//   state = {
+//     isShow: true,
+//   };
 
-  handleShowHide() {
-    this.setState({
-      isShow: !this.state.isShow,
-    });
-  }
+//   handleShowHide() {
+//     this.setState({
+//       isShow: !this.state.isShow,
+//     });
+//   }
 
-  render() {
-    const { listUser } = this.props;
-    return (
-      <div>
+//   render() {
+//     const { listUser } = this.props;
+//     return (
+//       <div>
+//         <div>
+//           <span
+//             className='btn'
+//             onClick={() => {
+//               this.handleShowHide();
+//             }}
+//           >
+//             {this.state.isShow ? 'Hide' : 'Show'}
+//           </span>
+//         </div>
+
+//         {this.state.isShow && (
+//           <div>
+//             {listUser.map((user) => {
+//               return (
+//                 <div
+//                   key={user.id}
+//                   className={+user.age > 18 ? 'text-red' : 'text-green'}
+//                 >
+//                   <div>
+//                     <h3>My name is {user.name}</h3>
+//                     <p>Age: {user.age}</p>
+//                   </div>
+
+//                   <div>
+//                     <span
+//                       className='btn btn--delete'
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </span>
+//                   </div>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+const DisplayInfor = (props) => {
+  const { listUser } = props;
+
+  return (
+    <div>
+      {true && (
         <div>
-          <span
-            className='btn'
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShow ? 'Hide' : 'Show'}
-          </span>
-        </div>
-
-        {this.state.isShow && (
-          <div>
-            {listUser.map((user) => {
-              return (
-                <div
-                  key={user.id}
-                  className={+user.age > 18 ? 'text-red' : 'text-green'}
-                >
-                  <div>
-                    <h3>My name is {user.name}</h3>
-                    <p>Age: {user.age}</p>
-                  </div>
-
-                  <div>
-                    <span
-                      className='btn btn--delete'
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </span>
-                  </div>
+          {listUser.map((user) => {
+            return (
+              <div
+                key={user.id}
+                className={+user.age > 18 ? 'text-red' : 'text-green'}
+              >
+                <div>
+                  <h3>My name is {user.name}</h3>
+                  <p>Age: {user.age}</p>
                 </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+
+                <div>
+                  <span
+                    className='btn btn--delete'
+                    onClick={() => props.handleDeleteUser(user.id)}
+                  >
+                    Delete
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
